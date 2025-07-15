7fdf981c4488bcefec9176aead85ede375cb3980
kaurine1 <a.kalisa@alustudent>
2025-07-15 03:12:54 -0700

    API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -21,7 +21,9 @@ if __name__ == "__main__":
 
     done_tasks = [task for task in todos if task.get("completed")]
     total_tasks = len(todos)
+   print(
+    f"Employee {emp_name} is done with tasks({len(done_tasks)}/{total_tasks}):"
+)
 
-    print(f"Employee {emp_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
     for task in done_tasks:
         print(f"\t {task.get('title')}")
